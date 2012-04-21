cd7c0f1cdf00467fb79252f7e8d035dec2835227
geistteufel <geistteufel@yahoo.fr>
2012-04-21 22:02:16 +0400

    Upgrade Base Store for other DB engine with Redis based method

--- lib/Celogeek/SCK/Store/Base.pm
+++ lib/Celogeek/SCK/Store/Base.pm
@@ -18,6 +18,22 @@ sub _die {
 #method need to be implemented
 for my $meth (qw/
         validate_connection
+        get_by_url
+        set_by_url
+        exists_by_url
+        increment_by_url
+        get_by_shorturl
+        set_by_shorturl
+        exists_by_shorturl
+        increment_by_shorturl
+        increment_top10_by_url
+        increment_stat
+        increment_top10_by_shorturl
+        url_from_shorturl
+        top10
+        reset_top10
+        all_by_url
+        del_by_url
     /) {
     eval <<EOF
     sub $meth { shift->_die }
